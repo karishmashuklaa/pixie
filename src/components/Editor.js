47a65f33a0caf1a566ffd1f6bd32eaaeeb395bec
karishmas6 <carishmashukla28@gmail.com>
2021-07-15 02:15:58 +0530

    Layout

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,22 @@
 import React, { useState } from 'react'
 
 const Editor = () => {
+    const [panelWidth, setPanelWidth] = useState(20)
+    const [panelHeight, setPanelHeight] = useState(20)
+    const [hideOptions, setHideOptions] = useState(false)
+    const [hideDrawingPanel, setHideDrawingPanel] = useState(true)
+    const [buttonText, setButtonText] = useState("Start Drawing")
+    const [selectedColor, setSelectedColor] = useState("#00000")
+
+    const initializeDrawingPanel = () => {
+        setHideOptions(!hideOptions)
+        setHideDrawingPanel(!hideDrawingPanel)
+
+        buttonText === "Start Drawing" 
+            ? setButtonText("Reset")
+            : setButtonText("Start Drawing") 
+    }
+
     return (
         <div id="editor">
            <h1>Pixel Editor</h1>
@@ -9,17 +25,19 @@ const Editor = () => {
            <span>Width</span>
                <div className="option">
                    <input type="number" className="panelInput" 
-                   defaultValue="10"
+                   defaultValue={panelWidth}
+                   onChange={(e) => setPanelWidth(e.target.value)}
                    />
                </div>
                <span>Height</span>
                <div className="option">
                    <input type="number" className="panelInput" 
-                   defaultValue="10"
+                   defaultValue={panelHeight}
+                   onChange={(e) => setPanelHeight(e.target.value)}
                    />
                 </div>
            </div>
-           <button className="button">Start Drawing</button>
+           <button className="button">{buttonText}</button>
         </div>
     )
 }
